ea4363abaf2b22681d1c99aa8d19039249f45ffc
shadin <shadinraj20@gmai.comecho #>
2021-01-22 11:22:50 +0600

    second part complite

--- assignment.js
+++ assignment.js
@@ -1,3 +1,31 @@
-fanction KilometerToMeter (){
-    
-}
+    // // first function
+    // function kilometerToMeter (meter){
+    //     var kilometer = meter/1000;
+    //     return kilometer;
+    // }
+    // var khulnaToDhaka = kilometerToMeter (127000);
+    // console.log(khulnaToDhaka);
+        
+   // second function
+   function budgetCalculator(watch, phone, laptop){
+   var product = 0;
+   if(watch<=10){
+    product = watch * 50;
+   }else if(watch<=10){
+    var watch = 10*50;
+    var remaining = watch-10; 
+    var phone = remaining * 100;
+    product = watch + phone;
+   }else{
+    var watch = 10*50;
+    var phone = 10*100;
+    var remaining = phone-20;
+    var laptop =  remaining *300;
+    product = watch + phone + laptop;
+   }
+    return product;
+   }
+   var result = budgetCalculator(20,  20, 15);
+   console.log(result);
+   
+  
